87013293f7757ec4e3f771fc5e684854b5167709
chao lobi <gogoiladitya@gmail.com>
2023-03-12 12:22:55 +0530

    responsive home page is created

---
+++ script.js
@@ -0,0 +1,12 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+
+bar.addEventListener('click', () => {
+    nav.style.right = '0px';
+})
+
+close.addEventListener('click', () => {
+    nav.style.right = '-300px';
+})
